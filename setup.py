33b9684270392cde509da0eaa21695528e050ee3
Praveen Soni <sonipraveen255@gmail.com>
2025-02-13 01:28:10 +0530

    'setup'

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+def get_requirements(file_path:str):
+    '''
+    This function will get the list of requirements
+    '''
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n', '') for req in requirements]
+
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+    return requirements
+
+setup(
+    name='ML Project',
+    version='1.0',
+    author='Praveen Soni',
+    author_email='sonipraveen255@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
